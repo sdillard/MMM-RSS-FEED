a761bb0f8c8202a9117c56357337b425058c6e20
Spencer Dillard <spencer.dillard@gmail.com>
2021-12-13 17:41:13 -0800

    added configuration for title & number of entries and also dedupe entries

--- MMM-RSS-FEED.js
+++ MMM-RSS-FEED.js
@@ -13,18 +13,45 @@ Module.register("MMM-RSS-FEED",{
 		Log.info("Starting module: " + this.name);
 	},
 	
+	// TODO: Allow for array of feeds
+	// TODO: Create timer to refresh feeds
+	// TODO: Create class for feed heading
+	// TODO: Allow more sizing options
+	// TODO: Allow extraction of other elements in config
 	getDom: function () {
 		var wrapper = document.createElement("div");
     		var url = this.config.url;
+		var feedTitle = "";
+		var maxEntries = 10;
+		if (this.config.hasOwnProperty('feedTitle')) {
+			feedTitle = this.config.feedTitle;
+		}
+		if (this.config.hasOwnProperty('maxEntries')) {
+			maxEntries = parseInt(Number(this.config.maxEntries));
+		}
 		feednami.load(url,function(result){
 			if(result.error) {
 				console.log(result.error);
 			} else {
+				var content = "";
+				content = "<b>" + feedTitle + "</b>";
 				var entries = result.feed.entries;
-				for(var i = 0; i < entries.length; i++){
-				var entry = entries[i];
-				wrapper.innerHTML = entry.title;
+				var processedEntries = [];
+				var entryCount = entries.length;
+				console.log("MMM-RSS-FEED: entryCount" + entryCount);
+				if (entryCount > 0) {
+					for (var i = 0; i < entryCount; i++) {
+						var entry = entries[i];
+						if (!(processedEntries.includes(entry.title))) {
+							content = content + "<br/>" + entry.title;
+							processedEntries.push(entry.title);
+						}
+						if (processedEntries.length > maxEntries) {
+							break;
+						}
+					}
 				}
+				wrapper.innerHTML = content;
 			}
 		});
 		return wrapper;
